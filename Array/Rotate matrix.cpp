a08fe3577668448a5c98b320f14788557e371f1a
ChetnaNSharma <chetna.ns.sharma@gmail.com>
2021-08-12 17:01:24 +0530

    Rotate matrix by 90 degree

---
+++ Array/Rotate matrix.cpp
@@ -0,0 +1,16 @@
+void Solution::rotate(vector<vector<int> > &A) {
+
+    int n {A.size()};
+
+    for(int i {0}; i<n; i++)
+    {
+        for(int j {i}; j<n; j++)
+        {
+        swap(A[i][j], A[j][i]);
+        }
+    }
+    for(int i {0}; i<n; i++)
+    {
+    reverse(A[i].begin(), A[i].end());
+    }
+}
